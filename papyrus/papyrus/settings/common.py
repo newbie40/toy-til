6f83e68158ba2dced6e90834590c37d55205ac37
newbie40 <gwangsuda@gmail.com>
2018-12-13 16:21:45 +0900

    모델 추가

--- papyrus/papyrus/settings/common.py
+++ papyrus/papyrus/settings/common.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'til.apps.TilConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- papyrus/til/models.py
+++ papyrus/til/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        print('%s %s' % self.user.username, self.name)
+
+
+class Post(models.Model):
+    content = models.TextField()
+    bookmark = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        print('%s %s %s %s' % self.user.username, self.category.name, self.content[:30], self.bookmark)
